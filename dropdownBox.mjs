17c4f90df433b47430b625c6b6cb18b0eba3cb51
robert <kaukana0@tuta.io>
2023-04-21 11:44:39 +0200

    prepare API implementation

--- dropdownBox.mjs
+++ dropdownBox.mjs
@@ -1,50 +1,57 @@
 import MarkUpCode from  "./markUpCode.mjs"		// keep this file html/css free
 
+/*
+TODO:
+- get selected elements
+- pre-select
+- single-select
+- disable search
+- close
+- clearall button,
+- have min. 1 selected
+- max selection
+- locking
+- when moving in DOM, move also ECL-generated HTML
+- on click, give just now clicked key/vaue to onselect callback
+- implement setSelectedByKey
 
+All the above implement the API of the IPCT dropdownBox, 
+so that this can be used (almost) as a drop-in replacement.
+Plus, some ECL select specific functionality.
+*/
+
+
+
+// please note comments of "get selected" and "set data".
 class Element extends HTMLElement {
 
-	#_callback		// function; from an attribute
-	#_maxSelections	// from an attribute
-	#_isLocked		// if true, user can't influece selection and no callback will be invoked
+	#_cfg
 
 	#$(elementId) { return this.getElementById(elementId)	}
 
-	constructor() {
-		super()
-		this.#_isLocked = false
-		this.#_maxSelections = 10
-	}
+	constructor() {	super(); this.#_cfg={} }
 
-	#_registerEvents() {
-		const argl = this.nextElementSibling.querySelectorAll("div [class='ecl-checkbox']")
-		argl.forEach(e=>e.addEventListener("click", e=>this.#invokeCallback("","")))		//TODO
-	}
+	connectedCallback() {	}
+	disconnectedCallback() {console.debug("dropdownBox: disconnected") }
 
-	connectedCallback() {
-		this.#_maxSelections = this.hasAttribute('maxSelections') ? this.getAttribute('maxSelections') : 10
-	}
+	// in this component, props are reflected to attributes, so that 
+	// these things can be set from markup as well as from JS.
+	// this makes sense only for values which are naturally of type string.
+	set onSelect(val) { this.#_cfg.onSelectCallback = val }
+	set selected(val) { this.#_cfg.selected = val }
+	set maxSelections(val) { this.setAttribute('maxselections', val) }
+	set minSelections(val) { this.setAttribute('minselections', val) }
+	set multiselect(val) { this.setAttribute('ismultiselect', val) }
+	set searchenabled(val) { this.setAttribute('searchenabled', val) }
+	set closeenabled(val) { this.setAttribute('closeenabled', val) }
+	set clearallenabled(val) { this.setAttribute('clearallenabled', val) }
+	set locked(val) { this.setAttribute('locked', val) }
 
-	disconnectedCallback() {
-		console.debug("dropdownBox: disconnected")
-	}
-bla
-	set data(val) {
-		this.appendChild( MarkUpCode.getHtmlTemplate(MarkUpCode.getHtml(val)).cloneNode(true) )
-		setTimeout(()=> {
-			this.bla = new ECL.Select(this.firstElementChild)
-			this.bla.init()
-			this.#_registerEvents()
-		}, 100)
-	}
-
-	set callback(val) {
-		this.#_callback = val
-	}
-
-	set maxSelections(val) {
-		this.setAttribute('maxSelections', val)
-	}
-
+	// there's HTML which you wrote (<ecl-like-select> which creates a <select> as a child),
+	// and after calling init(),
+	// another HTML content (a <div>) is being generated (by ECL js code) as the next sibling.
+	// your HTML is being hidden while the other is shown.
+	// the selection information however is in your HTML content, not in the generated one.
 	get selected() {
 		const retVal = new Map()
 		this.querySelectorAll("select option[selected]").forEach( e=> {
@@ -53,33 +60,62 @@ bla
 		return retVal
 	}
 
-	setLocked(isLocked) {
-		this.#_isLocked = isLocked
-	}
-
-	setSelectedByKey(key) {
+	// use this last, because after this no changes
+	// can be made anymore to content, pre-selection etc.
+	// this is because of the way the ECL-Select works - once the
+	// additional HTML is created there's no (regular) way to change it anymore.
+	// note also: ECL doesn't work inside shadow DOM.
+	set data(val) {
+		this.appendChild( MarkUpCode.getHtmlTemplate(MarkUpCode.getHtml(this.#_cfg, val)).cloneNode(true) )
+		setTimeout(()=> {
+			new ECL.Select(this.firstElementChild).init()
+			this.#_registerEvents()
+		}, 100)
 	}
 
 	static get observedAttributes() {
-		return ['data', 'callback', 'maxselections']
+		return ['maxselections', 'minselections', 'locked',
+			'multiselect', 'searchenabled', 'closeenabled', 'clearallenabled' ]
 	}
 
 	attributeChangedCallback(name, oldVal, newVal) {
-		if (name === 'data' || name === 'callback') {
-			console.warn("dropdownBox: setting "+name+" via html attribute is being ignored. please use js property instead.")
+		switch(name) {
+			case 'maxselections':
+				this.#_cfg.maxSelections = parseInt(newVal)
+				break;
+			case 'minselections':
+				this.#_cfg.minSelections = parseInt(newVal)
+				break;
+			case 'multiselect':
+				this.#_cfg.ismultiselect = newVal
+				break;
+			case 'searchenabled':
+				this.#_cfg.searchenabled = newVal
+				break;
+			case 'closeenabled':
+				this.#_cfg.closeenabled = newVal
+				break;
+			case 'clearallenabled':
+				this.#_cfg.clearallenabled = newVal
+				break;
+			case 'locked':
+				this.#_cfg.isLocked = newVal
+				break;
+			default:
+				console.warn("dropdownBox: unknown attribute '"+name+"'.")
 		}
-		if(name === 'maxselections') {
-			if(newVal) {
-				this.#_maxSelections = parseInt(newVal)
-			}
-		}			
 	}
 
-	#invokeCallback(key,val) {
-		if(this.#_callback !== undefined) {
-			this.#_callback(key, val)
+	#_registerEvents() {
+		const argl = this.nextElementSibling.querySelectorAll("div [class='ecl-checkbox']")
+		argl.forEach(e=>e.addEventListener("click", e=>this.#invokeOnSelectCallback("","")))		//TODO
+	}
+
+	#invokeOnSelectCallback(key,val) {
+		if(this.#_cfg.onSelectCallback !== undefined) {
+			this.#_cfg.onSelectCallback(key, val)
 		} else {
-			console.debug("dropdownBox: No callback")
+			console.debug("eclLikeDropdown: No onSelect callback")
 		}
 	}
 }
--- markUpCode.mjs
+++ markUpCode.mjs
@@ -11,20 +11,20 @@ export default class MarkUpCode {
 		return t.content
 	}
 
-  static getHtml(data) {
-    let retVal = this.getIntro()
+  static getHtml(cfg, data) {
+    let retVal = this.getIntro(cfg)
     for (let [key, val] of data[0]) {
       retVal += `<option value="${key}">${val}</option>`
     }
     return retVal+this.getOutro()
   }
 
-  static getIntro() {
+  static getIntro(cfg) {
+    console.log(cfg, cfg.hasOwnProperty("ismultiselect"))
     return `
     <select
     class="ecl-select"
-    multiple
-    data-ecl-select-multiple
+    ${typeof cfg.ismultiselect!=="undefined"?"multiple data-ecl-select-multiple":""}
     data-ecl-select-default="Select&#x20;an&#x20;item" 
     data-ecl-select-search="Enter&#x20;keyword" 
     data-ecl-select-no-results="No&#x20;results&#x20;found" 
